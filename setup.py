315fce66d7f4043048966d11c4c6e03d5b1ba641
Essau Ramirez <saumotions@gmail.com>
2013-06-06 13:32:37 -0400

    Added the package requirements

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name='pysques',
     version='0.1dev',
-    packages=['pysqes',],
+    packages=find_packages(exclude=['tests', ]),
     license='',
+    install_requires=[
+        item for item in
+        open("requirements.txt").read().split("\n")
+    ],
     long_description=open('README.md').read()
 )
