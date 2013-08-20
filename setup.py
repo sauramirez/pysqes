bab4e047443bd0104a5ab9dc0ea2469dbd375b97
Essau Ramirez <saumotions@gmail.com>
2013-08-20 14:10:01 -0400

    Added the license and a better setup file.

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import find_packages
 
 setup(
     name='pysques',
-    version='0.1dev',
+    version='0.1',
+    description='A simple queue service using Amazon SQS and boto',
+    long_description=open('README.md').read(),
+    author='Essau Ramirez',
+    author_email='saumotions+pypi@gmail.com',
+    license='Apache',
+    keywords=('queue', 'amazon', 'distributed'),
     packages=find_packages(exclude=['tests', ]),
-    license='',
     install_requires=[
         item for item in
         open("requirements.txt").read().split("\n")
-    ],
-    long_description=open('README.md').read()
+    ]
 )
