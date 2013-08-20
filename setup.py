bb767654dd2058c085910d8f7e4f31d91f564cb8
Essau Ramirez <saumotions@gmail.com>
2013-08-20 14:48:24 -0400

    Adding package data to setup

--- setup.py
+++ setup.py
@@ -3,14 +3,17 @@ from setuptools import find_packages
 
 setup(
     name='pysqes',
-    version='0.1',
+    version='0.1.2',
+    license='Apache',
     description='A simple queue service using Amazon SQS and boto',
     long_description=open('README.md').read(),
     author='Essau Ramirez',
     author_email='saumotions+pypi@gmail.com',
-    license='Apache',
+    url='https://github.com/sauramirez/pysqes',
+    download_url='https://github.com/sauramirez/pysqes',
     keywords=('queue', 'amazon', 'distributed'),
     packages=find_packages(exclude=['tests', ]),
+    include_package_data = True,
     install_requires=[
         item for item in
         open("requirements.txt").read().split("\n")
